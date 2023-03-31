5d57bcdff30c693ef06fcd93e07fd188f2aacd37
Swen <94377376+SwenteMebel@users.noreply.github.com>
2023-03-31 16:35:28 +0200

    Add files via upload

---
+++ memory.js
@@ -0,0 +1,111 @@
+"use strict";
+
+class Card {
+    constructor(card1, card2 = card1, set = card1, sound = card1){
+        this.card1 = card1;
+        this.card2 = card2;
+        this.set = set;
+        this.sound = sound;
+    }
+}
+
+
+
+const myField = document.getElementById('field');
+const selecteer = document.getElementById('selecteer');
+
+myField.addEventListener('click', onClickCard);
+selecteer.addEventListener("change", onSelectFieldsize);
+let boardClass;
+let myCardSet;
+
+const myCardArray = ["duck", "kitten", "piglet", "puppy", "calf", "veal", "lamb", "rooster", "horse", 
+"mouse", "dog", "cat", "goose", "goat", "sheep", "pig", "cow", "chick", "hen"];
+
+
+
+//functie die myDblCardArray shuffelt
+function schudden(array){
+    var m = array.length, t, i;
+
+    while(m){
+        i = Math.floor(Math.random() * m--);
+
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t 
+    }
+    return array;
+};
+
+// selecteert die optie die de gebruiker kiest, aan de hand hievan word de class in css gekozen.
+function onSelectFieldsize(e){
+        let selectGrote = e.target.value;
+        let aangepasteArray = schudden(myCardArray);
+        switch(selectGrote){
+            case '4':
+                boardClass = 'board4';
+                aangepasteArray = myCardArray.slice(0,8);
+                break;
+
+            case '5':
+                boardClass = 'board5';
+                aangepasteArray = myCardArray.slice(0,12);
+                break;
+
+            case '6': 
+                boardClass = 'board6';
+                aangepasteArray = myCardArray.slice(0,18);
+                break;
+            default:
+                break;
+               
+        };
+
+        let myDblCardArray = aangepasteArray;
+        myDblCardArray = myDblCardArray.concat(aangepasteArray);
+        myDblCardArray = schudden(myDblCardArray); 
+        myCardSet = myDblCardArray.map(card => new Card(card));
+        populateField();
+
+};
+
+
+
+
+// bouwt de kaarten op, met div, img, en class
+function populateField(){
+myField.innerHTML = '';   
+myCardSet.forEach(card => {    
+ 
+let newTile = document.createElement('div');
+let newCard = document.createElement('img');
+let cover = document.createElement('img');
+let imageURL = 'img/' + card.card1 + '.jpg';
+
+newTile.setAttribute('class', boardClass);
+cover.setAttribute("src", "img/cover.png");
+cover.setAttribute('class', 'covered');
+newCard.setAttribute('src', imageURL);
+newCard.setAttribute('name', card.card1);
+
+newTile.appendChild(newCard);
+newTile.appendChild(cover);
+myField.appendChild(newTile);
+});
+
+
+}
+
+
+// geeft in console.log weer wat het is
+function onClickCard(e){
+    if (e.target.className === 'covered'){
+        e.target.className = 'uncovered';
+        console.log(e.target.parentNode.firstChild.getAttribute('name'))
+        
+    } 
+    
+   
+}
+
